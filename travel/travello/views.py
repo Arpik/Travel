66df3e1458889b8ae5d344c42cde76f496da9204
Arpik <arpiksargsyan@gmail.com>
2019-09-19 13:32:04 +0400

    Created dest1 object of Destination type

--- travel/travello/views.py
+++ travel/travello/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+
+    dest1 = Destination()
+    dest1.name = "Paris"
+    
+    return render(request, "index.html", {'dest1' : dest1})
+
+def about(request):
+    return render(request, "about.html")
