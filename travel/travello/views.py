35b442d28abdee7fd7a8da3ca182249447459088
Arpik <arpiksargsyan@gmail.com>
2019-09-19 14:59:39 +0400

    added img value to dest objects

--- travel/travello/views.py
+++ travel/travello/views.py
@@ -8,33 +8,39 @@ def index(request):
     dest1.name = "Paris"
     dest1.desc = "The most romantic city"
     dest1.price = 800
+    dest1.img = destination_1
 
     dest2 = Destination()
     dest2.name = "London"
     dest2.desc = "The City of lights"
     dest2.price = 900
+    dest2.img = destination_2
     
     dest3 = Destination()
     dest3.name = "Milan"
     dest3.desc = "The City of lights"
     dest3.price = 900
+    dest3.img = destination_3
     
     dest4 = Destination()
     dest4.name = "Rome"
     dest4.desc = "The City of history"
     dest4.price = 600
+    dest4.img = destination_4
 
     
     dest5 = Destination()
     dest5.name = "Amsterdam "
     dest5.desc = "The city of bikes"
     dest5.price = 500
+    dest5.img = destination_5
 
     
     dest6 = Destination()
     dest6.name = "Bacabal"
     dest6.desc = "The city of love..."
     dest6.price = 1900
+    dest6.img = destination_6
 
     dests = [dest1, dest2, dest3, dest4, dest5, dest6] 
 
