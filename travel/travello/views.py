c4e78885c4b961c5fe7476e3ee69003356ac552d
Arpik <arpiksargsyan@gmail.com>
2019-09-19 13:51:03 +0400

    Added dest1 to dest6 objects

--- travel/travello/views.py
+++ travel/travello/views.py
@@ -6,6 +6,11 @@ def index(request):
 
     dest1 = Destination()
     dest1.name = "Paris"
+    dest1.desc = "The most romantic city"
+    dest1.price = 800
+
+    dest2 = Destination()
+    
     
     return render(request, "index.html", {'dest1' : dest1})
 
