754674358cf4ac841411e7b56970c85ff3467ebf
Arpik <arpiksargsyan@gmail.com>
2019-09-19 13:53:46 +0400

    Added desc1 to desc6 objects

--- travel/travello/views.py
+++ travel/travello/views.py
@@ -9,8 +9,33 @@ def index(request):
     dest1.desc = "The most romantic city"
     dest1.price = 800
 
-    dest2 = Destination()
+    dest2 = Destination()]
+    dest2.name = "London"
+    dest2.desc = "The City of lights"
+    dest2.price = 900
     
+    dest3 = Destination()]
+    dest3.name = "Milan"
+    dest3.desc = "The City of lights"
+    dest3.price = 900
+    
+    dest4 = Destination()]
+    dest4.name = "Rome"
+    dest4.desc = "The City of history"
+    dest4.price = 600
+
+    
+    dest5 = Destination()]
+    dest5.name = "Amsterdam the city of bikes"
+    dest5.desc = "The City of lights"
+    dest5.price = 500
+
+    
+    dest6 = Destination()]
+    dest6.name = "Bacabal"
+    dest6.desc = "The city of love..."
+    dest6.price = 1900
+
     
     return render(request, "index.html", {'dest1' : dest1})
 
